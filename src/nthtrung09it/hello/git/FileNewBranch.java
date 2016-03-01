a5c96e3a50d5030ab0bb08ac0b0968f8f8641742
nthtrung09it <ThEtEaR@Nguyens-MacBook-Pro.local>
2016-03-01 20:44:13 +0700

    Add new file FileNewBranch

---
+++ src/nthtrung09it/hello/git/FileNewBranch.java
@@ -0,0 +1,19 @@
+package nthtrung09it.hello.git;
+
+/**
+ * @author TrungNTH
+ * @version 1.0
+ * @since 01/03/2016
+ */
+public class FileNewBranch {
+
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
